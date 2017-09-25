001e566918a09d5c881f9b8836ab01bc06df4bb5
mvleandro <mleandro@stone.com.br>
2017-09-25 12:48:52 -0300

    First commit.

---
+++ Consumer/Consumer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using System.Threading;
+
+namespace WorkshopRabbitMQ
+{
+    class Consumer
+    {
+        public static void Main()
+        {
+			// Criamos aqui a connection factory.
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", Port = 35672 };
+
+			// Instanciamos a conexão e o canal de comunicação com o RabbitMQ.
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+            {
+                // Criamos a fila caso não exista.
+                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+                // Fazemos o binding (a ligação) entre a exchange e a fila.
+                channel.QueueBind(queue: "task_queue", exchange: "task_exchange", routingKey: "", arguments: null);
+
+                Console.WriteLine(" [*] Waiting for messages.");
+
+                // Aqui declaramos o processo de consumo da mensagens.
+                EventingBasicConsumer consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+
+                    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+                };
+
+                // Iniciamos o processo de consumo das mensagens.
+                channel.BasicConsume(queue: "task_queue", autoAck: false, consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Producer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace WorkshopRabbitMQ
+{
+    class Producer
+    {
+        static void Main(string[] args)
+        {
+
+            // Criamos aqui a connection factory.
+            ConnectionFactory factory = new ConnectionFactory() { HostName = "localhost", Port = 35672 };
+
+            // Instanciamos a conexão e o canal de comunicação com o RabbitMQ.
+            using (IConnection connection = factory.CreateConnection())
+            using (IModel channel = connection.CreateModel())
+			{
+
+                // Declaramos explicitamente a exchange que iremos utilizar.
+                channel.ExchangeDeclare(exchange: "task_exchange",
+                                        type: "fanout",
+                                        durable: true,
+                                        arguments: null);
+
+                // Pegamos a mensagem a ser enviada.
+				string message = GetMessage(args);
+
+                // Necessário converter a mensagem de string para bytes.
+				byte[] body = Encoding.UTF8.GetBytes(message);
+
+                // Definimos que a mensagem enviada será persistente.
+                IBasicProperties properties = channel.CreateBasicProperties();
+				properties.Persistent = true;
+
+                // Enviamos, enfim, a mensagem para o RabbitMQ.
+				channel.BasicPublish(exchange: "task_exchange",
+									 routingKey: "",
+									 basicProperties: properties,
+									 body: body);
+
+                // Saída no console da mensagem enviada.
+				Console.WriteLine(" [x] Sent {0}", message);
+			}
+
+			Console.WriteLine(" Press [enter] to exit.");
+			Console.ReadLine();
+        }
+
+        /// <summary>
+        /// Help us to get a message from command line.
+        /// </summary>
+        /// <returns>The message.</returns>
+        /// <param name="args">Arguments.</param>
+		private static string GetMessage(string[] args)
+		{
+			return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+		}
+    }
+}
